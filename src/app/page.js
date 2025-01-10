a4b686e97f07a0faddf2cb0ac76e0a07540a0928
Abeen Khanal <abeenkhanal16@gmail.com>
2025-01-10 14:28:11 +0545

    homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Banner from "@/components/Banner/Banner";
 import Hero from "@/components/Hero/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className="bg-gradient-to-b from-blue-800 to-blue-900 text-white py-16 text-center">
-      <h1 className="text-3xl font-bold mb-4">Experience PharmaEase in Action</h1>
+    <div className="bg-gradient-to-r from-blue-800 to-blue-100 text-white py-16 text-center">
+      <h1 className="text-5xl font-bold mb-4">Experience PharmaEase in Action</h1>
       <p className="text-lg mb-8">
         Discover how PharmaEase can transform your pharmacy operations.
         <br />
