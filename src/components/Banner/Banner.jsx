c0bc6cc2a64d64705fde66b774ad7b477faa609e
Abeen Khanal <abeenkhanal16@gmail.com>
2025-01-21 13:06:46 +0545

    banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
   return (
-    <div className="bg-gradient-to-r from-blue-800 to-blue-100 text-white py-16 text-center">
+    <div className="bg-gradient-to-r from-blue-800 to-blue-100100 text-white py-16 text-center">
       <div className="w-11/12 max-w-4xl mx-auto">
         <h1 className="text-3xl sm:text-5xl font-bold mb-4">
           Experience Pharmacy Management System in Action
